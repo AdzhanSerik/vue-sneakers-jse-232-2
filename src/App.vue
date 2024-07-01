7aeaddabf059651dc7b10d959f28a83416e2f4be
AdzhanSerik <adzhan.serik@gmail.com>
2024-07-01 21:30:45 +0500

    First

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Sneakers from './components/SneakersCart.vue'
+</script>
+
+<template>
+  <!-- <MyHello /> -->
+  <div class="carts">
+    <Sneakers name="Кеды" brand="Nike" :price="25000" />
+    <Sneakers name="Сапоги" brand="Adidas" :price="70000" />
+    <Sneakers name="Сапоги" brand="Adidas" :price="70000" />
+    <Sneakers name="Сапоги" brand="Adidas" :price="70000" />
+    <Sneakers name="Сапоги" brand="Adidas" :price="70000" />
+    <Sneakers name="Сапоги" brand="Adidas" :price="70000" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 30px;
+  text-align: center;
+}
+.carts {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <h2>Привет Vue</h2>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+}
+</style>
