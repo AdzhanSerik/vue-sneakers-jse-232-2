2780abc0f62f83b81e93fa2eff12068dd145396c
AdzhanSerik <adzhan.serik@gmail.com>
2024-07-05 19:07:50 +0500

    asdsad

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup>
+import Header from './components/HeaderComponent.vue'
+import Sneaker from './components/SneakerSingle.vue'
 </script>
 
-<template></template>
+<template>
+  <div class="bg-white m-20 shadow-xl rounded-xl">
+    <Header />
+    <h2 class="p-10 text-2xl font-bold">Все кроссовки</h2>
+    <div class="p-10 grid grid-cols-4 gap-5">
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+      <Sneaker />
+    </div>
+  </div>
+</template>
 
 <style scoped>
 </style>
---
+++ src/components/SneakerSingle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="border p-8 border-slate-200 rounded-3xl relative cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img class="absolute top-4 left-4" src="/like-2.svg" alt="Like" />
+    <img src="/sneakers/sneakers-4.jpg" alt="Sneaker" />
+    <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+    <div class="flex justify-between items-center">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <span class="font-bold">75000 тенге</span>
+      </div>
+      <img src="/plus.svg" alt="" />
+    </div>
+  </div>
+</template>
